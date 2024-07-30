7bc950da4c9b030726fbad3d2b1720d50c07d101
hamsangjin <rhkstlash@naver.com>
2024-07-30 11:50:20 +0900

    first commit

---
+++ src/main/java/com/inconcert/InconcertApplication.java
@@ -0,0 +1,13 @@
+package com.inconcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InconcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InconcertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inconcert/InconcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inconcert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InconcertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
