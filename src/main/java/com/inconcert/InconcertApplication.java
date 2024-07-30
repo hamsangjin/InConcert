13fd3e8fa3c0f32d8eda32cfdd3f09cc00b2916b
Hxxjeong <heejeong3394@gmail.com>
2024-07-30 18:06:27 +0900

    [Feat]: BaseEntity 생성
    - 각 테이블에서 작성일/수정일 확인하기 위함

--- src/main/java/com/inconcert/InconcertApplication.java
+++ src/main/java/com/inconcert/InconcertApplication.java
@@ -2,8 +2,10 @@ package com.inconcert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InconcertApplication {
 
 	public static void main(String[] args) {
