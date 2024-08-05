67565c973f9587d3590543c2ab9d1e3bbf708db6
Hxxjeong <heejeong3394@gmail.com>
2024-08-05 15:26:11 +0900

    [Feat]: 네이버 로그인 구현
    - 핸드폰 번호 unique 설정 지움, 프로필 기본 설정
    - 일반 로그인 url 수정

---
+++ src/main/java/com/inconcert/global/auth/CustomNaverUser.java
@@ -0,0 +1,43 @@
+package com.inconcert.global.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomNaverUser implements OAuth2User, UserDetails {
+    private String username;
+    private Map<String, Object> attributes;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getName() {
+        return this.username;
+    }
+}
