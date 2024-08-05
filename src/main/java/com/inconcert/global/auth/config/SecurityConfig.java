c2e52021a109a9af187e09ebf480257de6cf6e78
hamsangjin <rhkstlash@naver.com>
2024-08-05 14:10:54 +0900

    [Feat]: header 검색 기능 구현
    - search-result html/css 파일 생성
    - 2자 이상 검색한 경우에만 검색 가능
    - /search 경로 인증 예외

--- src/main/java/com/inconcert/global/auth/config/SecurityConfig.java
+++ src/main/java/com/inconcert/global/auth/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/home", "/loginform", "/registerform", "/register", "/login").permitAll()
                         .requestMatchers("/user/**").permitAll()
                         .requestMatchers("/info/**", "/review/**", "/match/**", "/transfer/**").permitAll()
+                        .requestMatchers("/search").permitAll()
                         .requestMatchers("/api/**","/css/**", "/js/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
