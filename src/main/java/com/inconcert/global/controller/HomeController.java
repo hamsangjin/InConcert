89ad60c7e1ad7ef9c9d895ada7b81261101d00ef
hamsangjin <rhkstlash@naver.com>
2024-08-06 11:36:41 +0900

    [Style]: 게시판 검색 시 검색 정보 불러와서 유지
    - model에 정보 담아서 불러오는 방법 사용

--- src/main/java/com/inconcert/global/controller/HomeController.java
+++ src/main/java/com/inconcert/global/controller/HomeController.java
@@ -50,6 +50,7 @@ public class HomeController {
     @GetMapping("/search")
     public String search(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
         model.addAttribute("posts", homeService.findByKeyword(keyword));
+        model.addAttribute("Headerkeyword", keyword);
         return "search-result";
     }
 }
