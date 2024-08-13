16c249f731112067ec3d0c81d2d3ac3dd2a23956
hamsangjin <rhkstlash@naver.com>
2024-08-13 23:33:22 +0900

    [Refactor]: 회원가입 이메일 인증 시 알림 창 중복 출력 제거
    - redirect 로그인 폼 수정
    - 댓글 프로필 보기 css 오류 해결 추가

--- src/main/java/com/inconcert/domain/user/controller/UserController.java
+++ src/main/java/com/inconcert/domain/user/controller/UserController.java
@@ -1,19 +1,15 @@
 package com.inconcert.domain.user.controller;
 
-import com.inconcert.domain.user.dto.request.FindIdReqDto;
-import com.inconcert.domain.user.repository.UserRepository;
 import com.inconcert.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
     @GetMapping("/loginform")
     public String loginform() {
         return "/loginform";
@@ -27,7 +23,7 @@ public class UserController {
     // 아이디 찾기
     @GetMapping("/idform")
     public String idform() {
-        return "user/idform";
+        return "/user/findidform";
     }
 
     // 비밀번호 찾기
