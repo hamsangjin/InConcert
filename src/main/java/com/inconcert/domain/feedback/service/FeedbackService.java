7b682e80791f40db74e19cfc723cbd94bf91ee8d
hamsangjin <rhkstlash@naver.com>
2024-08-26 12:49:23 +0900

    [Feat]: 상호 평가 시 매너점수에 반영 로직 추가
    - 게시글 작성자만 평가 가능하던 문제 해결
    - 동행완료 버튼은 동행 게시판에만 나타나게 수정
    - 매너점수 출력하는 부분에 null처리 추가

--- src/main/java/com/inconcert/domain/feedback/service/FeedbackService.java
+++ src/main/java/com/inconcert/domain/feedback/service/FeedbackService.java
@@ -11,6 +11,7 @@ import com.inconcert.global.exception.PostNotFoundException;
 import com.inconcert.global.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +20,7 @@ public class FeedbackService {
     private final UserRepository userRepository;
     private final MatchRepository matchRepository;
 
-
+    @Transactional
     public void feedback(Long postId, Long reviewerId, Long revieweeId, int point){
         Post post = matchRepository.findById(postId)
                 .orElseThrow(() -> new PostNotFoundException(ExceptionMessage.POST_NOT_FOUND.getMessage()));
@@ -30,8 +31,17 @@ public class FeedbackService {
         User reviewee = userRepository.findById(revieweeId)
                 .orElseThrow(() -> new UserNotFoundException(ExceptionMessage.USER_NOT_FOUND.getMessage()));
 
-        Feedback feedback = new Feedback(point, reviewer, reviewee, post);
+        Feedback feedback = Feedback.builder()
+                .point(point)
+                .reviewer(reviewer)
+                .reviewee(reviewee)
+                .post(post)
+                .build();
 
+        // 피드백 저장
         feedbackRepository.save(feedback);
+
+        // 피드백에서 reviewee의 평균점수를 매겨서 reviewee의 mannerPoint에 반영
+        userRepository.updateMannerPointByRevieweeId(revieweeId);
     }
 }
