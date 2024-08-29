9df5e271a4a234d5e6574eb1ae8e4ae8d768fc87
hamsangjin <rhkstlash@naver.com>
2024-08-29 15:41:35 +0900

    [Refactor]: 패키지명 변경, init코드 sql파일로 분리(gitignore), user, report, like 도메인 최적화
    - 패키지명 global -> common
    - 모든 config파일 common/config/*에서 관리
    - 컨트롤러/서비스 로직 분리
    - InitCategory/Role Sql 파일로 분리
    - Test 및 Swagger 내용 직접 작성 후 강제 푸시

--- src/main/java/com/inconcert/InconcertApplication.java
+++ src/main/java/com/inconcert/InconcertApplication.java
@@ -3,6 +3,7 @@ package com.inconcert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
