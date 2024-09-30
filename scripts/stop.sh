b71a681160a94096f5b353ad8575efb0a6c03e3b
Hxxjeong <112850550+Hxxjeong@users.noreply.github.com>
2024-09-30 14:57:50 +0900

    [Docs]: stop.sh 파일 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,7 +6,7 @@ STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NouFound" >> $STOP_LOG
+  echo "서비스 NotFound" >> $STOP_LOG
 else
   echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
