0f67c2df2210a52785e4a2b8668050f2c7d21007
SangJin Ham <rhkstlash@naver.com>
2024-09-30 14:46:50 +0900

    [Docs]: stop.sh 파일 작성

---
+++ scripts/stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/spring-github-action"
+JAR="$ROOT_PATH/inconcert-0.0.1-SNAPSHOT.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NouFound" >> $STOP_LOG
+else
+  echo "서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi
