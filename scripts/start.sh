86804dbc6dc04dc6fcf6239e196eadd8c7a171cb
Hxxjeong <heejeong3394@gmail.com>
2024-10-02 14:41:26 +0900

    [Docs]: 환경변수 파일 생성 후 적용

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source /home/ubuntu/env_vars.sh
+
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/inconcert-0.0.1-SNAPSHOT.jar"
 
@@ -8,6 +10,9 @@ ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
+
+echo "현재 사용자: $(whoami)" >> $START_LOG
+echo "secretKey: $(secretKey)" >> $START_LOG
 
 echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/inconcert-0.0.1-SNAPSHOT.jar $JAR
