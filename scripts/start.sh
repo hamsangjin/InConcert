6c3bf216894cb63169a917ca020a8c85ea957207
SangJin Ham <rhkstlash@naver.com>
2024-10-02 13:48:49 +0900

    [Docs]: 로그인 셸로 스크립트 실행

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source ~/.bashrc
-
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/inconcert-0.0.1-SNAPSHOT.jar"
 
@@ -15,7 +13,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/inconcert-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup bash -l -c "java -jar $JAR" > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
