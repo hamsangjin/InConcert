3b0c1c6c15e1a00df53ad644cf186eb987e7e672
SangJin Ham <rhkstlash@naver.com>
2024-10-02 14:07:22 +0900

    [Docs]: 자바 빌드 명령어 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/inconcert-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup bash -l -c "java -jar $JAR" > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
