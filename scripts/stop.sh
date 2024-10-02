0a14edd2b97932cfcd0ba9ad43789f0850e32a42
Hxxjeong <heejeong3394@gmail.com>
2024-10-02 14:46:03 +0900

    [Docs]: ubuntu 사용자 명시 제거

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,10 +5,12 @@ JAR="$ROOT_PATH/inconcert-0.0.1-SNAPSHOT.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
+NOW=$(date +%c)
+
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NotFound" >> $STOP_LOG
+  echo "[$NOW] 서비스 NotFound" >> $STOP_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
+  echo "[$NOW] 서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
