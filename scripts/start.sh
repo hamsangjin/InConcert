f35f215fb26e8bc83334e900000ebfb649c5f7c6
Hxxjeong <heejeong3394@gmail.com>
2024-10-02 14:54:28 +0900

    [Docs]: UTC에서 Seoul 시간으로 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,14 +11,11 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "현재 사용자: $(whoami)" >> $START_LOG
-echo "secretKey: $(secretKey)" >> $START_LOG
-
 echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/inconcert-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -Duser.timezone=Asia/Seoul -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
